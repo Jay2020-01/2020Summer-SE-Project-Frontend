0fef706dc74e7e18a6a55bfc34a3de7020e530d9
qinglianyan <921049836@qq.com>
2020-08-18 10:36:20 +0800

    templates changes

--- frontend/src/components/working_table/templates/4.vue
+++ frontend/src/components/working_table/templates/4.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <el-row>
-            <el-col style="float:center;"><div><h2>梦想计划清单</h2></div></el-col>
+        <!-- <el-row>
+            <el-col style="float:center;"><div><h2>个人简历模板</h2></div></el-col>
             <el-col style="float:center;position:relative;left:290px;">
                 <div>
                     <el-button @click="back">返回</el-button>
@@ -12,42 +12,57 @@
         <div style="float:center;position:relative;left:-270px;">
             <h4 style="display:inline-block;">模板亮点:</h4>
             <h6 style="display:inline-block;">比较好用,使用之后能够明显提高成绩</h6>
-        </div>
+        </div> -->
 
-        <!-- 梦想计划清单 -->
-        <div id="title">
-        <el-container style="background-color:#ebf4fc;">
-            <el-header style="border-width:0 0 5px 0;border-style:solid;border-color:#6eaad7;">
-                <h2 style="color:#6eaad7">梦想计划清单</h2>
-            </el-header>
-            <el-main>
-                <el-container>
-                    <el-header>
-                        <h3 style="color:#6eaad7;text-align:left;border-width:0 0 3px 0;border-style:solid;border-color:#6eaad7;">2021年</h3>
-                    </el-header>
-                    <el-main style="height:200px;">
-                        <h5 style="color:#6eaad7;text-align:left;">年度计划：</h5>
-                    </el-main>
-                </el-container>
-                <el-container>
-                    <el-header>
-                        <h3 style="color:#6eaad7;text-align:left;border-width:0 0 3px 0;border-style:solid;border-color:#6eaad7;">2023年</h3>
-                    </el-header>
-                    <el-main style="height:200px;">
-                        <h5 style="color:#6eaad7;text-align:left;">三年计划：</h5>
-                    </el-main>
-                </el-container>
-                <el-container>
-                    <el-header>
-                        <h3 style="color:#6eaad7;text-align:left;border-width:0 0 3px 0;border-style:solid;border-color:#6eaad7;">2025年</h3>
-                    </el-header>
-                    <el-main style="height:200px;">
-                        <h5 style="color:#6eaad7;text-align:left;">五年计划：</h5>
-                    </el-main>
-                </el-container>
-            </el-main>
-        </el-container>
+        <div>
+            <!-- 梦想计划清单 -->
+            <div class="total" style="text-align:center;">
+                <div class="biaoti" style="border-width:0 0 5px 0;border-style:solid;border-color:#6eaad7;">
+                    <h2 style="color:#6eaad7">梦想计划清单</h2>
+                </div>
+                <div class="biaoti2">
+                    <div class="class1">
+                        <h1>年度计划</h1>
+                    </div>
+                    <div class="content1">
+                        <ol>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="biaoti2">
+                    <div class="class1">
+                        <h1>三年计划</h1>
+                    </div>
+                    <div>
+                        <ol>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="biaoti2">
+                    <div class="class1">
+                        <h1>五年计划</h1>
+                    </div>
+                    <div>
+                        <ol>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                            <li class="paragraph"><h3></h3></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
         </div>
+        
     </div>
 </template>
 <script>
@@ -75,5 +90,18 @@
     width:780px;
     margin: 0 auto;
     border: 1px solid #cce0f1;
+}
+.class1{
+    color:#6eaad7;
+    text-align:left;
+    border-width:0 0 3px 0;
+    border-style:solid;
+    border-color:#6eaad7;
+    width: 700px;
+    /* background-color: blue; */
+}
+.biaoti2{
+    width: 700px;
+    margin: 0 auto;
 }
 </style>
