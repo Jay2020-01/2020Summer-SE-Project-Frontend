9a454bac9715305eb184b20051666cd5630232d9
qinglianyan <921049836@qq.com>
2020-08-19 17:21:13 +0800

    startoff.vue

---
+++ frontend/src/components/working_table/templates/startoff.vue
@@ -0,0 +1,194 @@
+<template>
+<div id="title">
+    <p style="text-align:center;color:red;border-bottom:1px solid #DCDFE6;border-top:1px solid #DCDFE6;"><br></p>
+    <p></p>
+    <div style="text-align: center;"><span
+        style="font-weight: bold; font-family: 宋体; font-size: x-large;">钻石文档-新手上路指南
+        </span>
+    </div>
+    <div style="text-align: center;">
+        <span style="font-weight: bold; font-family: 宋体; font-size: x-large;"><br></span>
+    </div>
+    <div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+    <span style="font-family: 宋体;">
+        <div style="width:780px;text-align:left;">
+            钻石文档是我们小组在小学期的一个小项目，目的是能够让你和你的团队能够一起分享、查看同一个文档，实现团队之间更加有效率的团队协作。本文旨在为广大新用户提供一个快速上手了解钻石文档的过程。<br>
+        </div>
+        <ul>
+            <li style="text-align:left"><span style="font-family: 宋体;">登录、注册</span></li>
+        </ul>
+        <div>
+            这是我们的登陆界面，你可以在这里新建属于你自己的钻石文档账号，从这里开始你的钻石文档的使用之旅。<br>
+        </div>
+        <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="https://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/bqQfVz5yrrGYSXMvKr.cqW24v.9QynTgZjtoOzGeyPGTV50pfwcGEynOqgc8CDdUAVXtySrNtscpZqhbcAyHwzWp5pI.wGAkGkilxA.898I!/b&bo=gAeRAwAAAAADBzc!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </span>
+ <ul>
+   <li style="text-align:left;"><span style="font-family: 宋体;">工作界面</span></li>
+ </ul>
+ 
+    <div>&nbsp; &nbsp; &nbsp; &nbsp; </div>
+    <div style="width:780px;text-align:left;">
+        <span
+            style="font-family: 宋体;">1、你的工作台会包括你最近浏览的文件，以及你的创建与收藏，在这里你可以快速浏览你常用与喜欢的文档内容。
+        </span>
+    </div>
+    <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrELYh9Ndz.2WRKr0IRtQJ3q30T3oUujEmErcIcOeVKS8bg44hjmcxRUI3*UWZRqMXONn7KNVb36wzbtngVv1hnkg!/b&bo=gAeUAwAAAAADFyI!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">2、收件箱会收到来自其他人的分享文件（也许你在这里还能收到告白消息），当然你也可以在这里进行相关的文档操作。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEOA52Ieu6XNrj8w36NQonv5ECKdKeQpEEbu9BxFtR65etwlIkdqLF3hln56yprdC3hfQOE*ZMYMIaMuSSR814H8!/b&bo=gAeKAwAAAAADFzw!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">3、我的桌面也有着最近浏览的功能，你可以把它当成自己另一个操作界面，因为二者的风格是有差异的。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEBtHlwt6DJlpFVX61k49wpt.3Rg5NnYtbvRjJpI0dqaLCnQu1rMJihlL1bxk7*IbTWffVOsVDKLUZ.4mTI0xFhQ!/b&bo=gAeSAwAAAAADFyQ!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">4、团队空间可以打开你所在的团队界面，在团队界面你可以查看分享团队文档，如果有管理权限还可以在团队管理界面邀请其他人以及进行相关团队设置。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEJGxB2DqNOpw1wr1WuRLnjXpSLgY79a3gTHtLUwUKL.hfRvfVGYJnqTeeNAWxxPc6cfmxF8MlNwbbMdt0NNK*6g!/b&bo=gAeRAwAAAAADFyc!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">5、最后是回收站，在这里可以看到你删除过的文件信息，点击清空可以将它们全部清除。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEJ6dzCqOz0sw0aPfsMtvACR0*n9Tjif2vIPyaN4Mk13VygvSOspw*tfaTbRGbr9uDKKOsD4Er6eLRzOhMes.xKA!/b&bo=gAeOAwAAAAADFzg!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">6、还有文档编辑界面，你可以在这里进行编辑、收藏、保存，如果你有权限的话还可以进行评论，我们的富文本编辑框为你提供了多种编辑方式，加油探索一下吧。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEEzVmi4HLGygfGabz8ekfSeDZP3MnO8zxdVZOFn4kXNOYd0yYvJsVtIQtynvJ2grnF4N*W0KbSSlfklVYZnmZXs!/b&bo=gAeSAwAAAAADFyQ!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+ 
+ <div>
+   <div style="text-align: center;"><br></div>
+   <ul>
+     <li style="text-align:left;"><span style="font-family: 宋体;">其他相关</span></li>
+   </ul>
+
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">1、还有右上角的个人信息，你可以在这里修改自己的个人信息，也可以自己上传自己个性化的头像。
+            </span>
+        </div>
+        <!-- 图片 -->
+    <div style="text-align: center;"><br>
+            <div>
+                <p>
+                <img class="pic" src="http://m.qpic.cn/psc?/V52enkk20GWyuS4fzJy41rPXx12MpmZJ/TmEUgtj9EK6.7V8ajmQrEKsceMFf.i84N2hM2kM6oprKDMc.uWGx0hgCT0IGURlezYJccUavHPevyoW1bDpKWjGZos*19vG1sR7TQzPqZAM!/b&bo=gAePAwAAAAADNxk!&rf=viewer_4">
+                </p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div style="text-align:left;"><br></div>
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div style="width:780px;text-align:left;">
+            <span
+                style="font-family: 宋体;text-align:left;">2、如果你在使用钻石文档的时候遇到了问题……
+            </span>
+            <div style="margin-left:30px;">&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span
+                    style="font-family: 宋体;">欢迎联系我们<br></span>&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span
+                    style="font-family: 宋体;">祝你使用愉快:）</span><br>
+            </div>
+
+        </div>
+    </div>
+   
+ </div>
+ </div>
+</template>
+
+<style>
+.pic{
+    width:780px;
+    height:auto;
+}
+#title{
+    width:780px;
+    margin: 0 auto;
+    border: 1px solid #cce0f1;
+}
+</style>
